17f627dcda4bc07eb6b89fbfa8f1c4efa9b72fc9
Li <jason.li@fiserv.com>
2023-11-01 15:39:51 -0400

    fin

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.transform.Source;
@@ -12,13 +13,20 @@ class Node<T> {
   }
 }
 
+
+
 class Solution {
   public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+    List<String> result = new ArrayList<String>();
+
+    while (head != null) {
+        result.add(head.val);
+        head = head.next;
+    }
+    return result;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -29,7 +37,7 @@ class Solution {
 
     // a -> b -> c -> d
 
-    Solution.linkedListValues(a); 
+    System.out.println(Solution.linkedListValues(a).toString()); 
     // -> [ "a", "b", "c", "d" ]
   }
 }
